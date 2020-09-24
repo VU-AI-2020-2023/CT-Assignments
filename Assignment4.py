734084834530bee2fa32c1b74dc9a99ebc7a0ed9
Mark Melnic <markhappy200@gmail.com>
2020-09-24 16:14:01 +0200

    Variables renamed

--- Assignment4.py
+++ Assignment4.py
@@ -1,22 +1,22 @@
 # Question 2
 coordinates = [("f", 9), ("z", 2), ("t", 4), ("x", 8), ("b", 1), ("m", 7)]
-list_A = [coord[0] for coord in coordinates]
-list_A.sort()
-sorted_A = []
-for item in list_A:
+first_list = [coord[0] for coord in coordinates]
+first_list.sort()
+first_sorted = []
+for item in first_list:
     for coord in coordinates:
         if coord[0] == item:
-            sorted_A.append(coord)
-print(sorted_A)
+            first_sorted.append(coord)
+print(first_sorted)
 
-list_B = [coord[1] for coord in coordinates]
-list_B.sort()
-sorted_B = []
-for item in list_B:
+second_list = [coord[1] for coord in coordinates]
+second_list.sort()
+second_sorted = []
+for item in second_list:
     for coord in coordinates:
         if coord[1] == item:
-            sorted_B.append(coord)
-print(sorted_B)
+            second_sorted.append(coord)
+print(second_sorted)
 
 # Question 4
 from math import log2
@@ -24,8 +24,8 @@ from math import log2
 elements = 500000
 quicksort = elements * elements
 mergesort = elements * log2(elements)
-times_faster = quicksort / mergesort
-print("Quicksort is %i times faster than merge sort." % times_faster)
+division = quicksort / mergesort
+print("Quicksort is %i times faster than merge sort." % division)
 
 # Question 6
 sentence = input("Write a sentence: ")
